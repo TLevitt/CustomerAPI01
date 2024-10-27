d6e7ae2dc7f31d9116b37587e610abf5bc499172
Trevor Levitt <trevor.levitt@gmail.com>
2024-10-27 11:01:19 -0400

    Adding test project

---
+++ CustomerAPI01.Test/UnitTests/CrudTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using FluentAssertions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomerAPI01.Test.UnitTests
+{
+    public class CrudTests
+    {
+
+        public CrudTests()
+        {
+        }
+
+        [Fact]
+        public void CreateCustomerTest()
+        {
+        }
+
+        [Fact]
+        public async Task GetCustomersTest()
+        {
+        }
+
+        [Fact]
+        public async Task GetCustomerTest()
+        {
+        }
+
+        [Fact]
+        public async Task UpdateCustomerTest()
+        {
+        }
+
+        [Fact]
+        public async Task DeleteCustomerTest()
+        {
+        }
+
+    }
+}
