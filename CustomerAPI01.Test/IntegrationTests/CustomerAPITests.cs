b0186c719d695fc43deb7fb4ed3e95716b79d680
Trevor Levitt <trevor.levitt@gmail.com>
2024-10-27 11:49:42 -0400

    Integration tests

--- CustomerAPI01.Test/IntegrationTests/CustomerAPITests.cs
+++ CustomerAPI01.Test/IntegrationTests/CustomerAPITests.cs
@@ -1,12 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CustomerAPI01.Data;
+using CustomerAPI01.Data.DTO;
+using FluentAssertions;
+using System.Net.Http.Json;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Xunit;
+using Assert = Xunit.Assert;
 
 namespace CustomerAPI01.Test.IntegrationTests
 {
-    internal class CustomerAPITests
+    public class CustomerAPITest(CustomerAPIFactory<Program> factory) : IClassFixture<CustomerAPIFactory<Program>>
     {
+        private readonly HttpClient _client = factory.CreateClient();
+
+        [Fact]
+        public async Task CreateCustomerTest()
+        {
+            var newCustomer = new CustomerDto()
+            {
+                EmailAddress = "test@testing.com",
+                FirstName = "Test",
+                LastName = "Testing",
+                MiddleName = "Tested",
+            };
+
+            var phoneNumber = new PhoneNumberDto()
+            {
+                Number = "1234567890",
+                Type = "Mobile",
+            };
+
+            newCustomer.PhoneNumbers = new List<PhoneNumberDto> { phoneNumber };
+
+            var jsonContent = JsonSerializer.Serialize(newCustomer);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("/api/Customer", content);
+            if (response.IsSuccessStatusCode)
+            {
+                response.Should().NotBeNull();
+                var createResponse = await response.Content.ReadAsStringAsync();
+                createResponse.Should().NotBeNull();
+            }
+            else
+            {
+                var errorResponse = await response.Content.ReadAsStringAsync();
+                Assert.Fail("Api call failed.");
+            }
+        }
+
+        [Fact]
+        public async Task ReadCustomerTest()
+        {
+            var testCustomer = TestData.Current.CustomerDbContext.Customers.First();
+
+            var response = await _client.GetFromJsonAsync<Customer>($"/api/Customer/{testCustomer.CustomerId}");
+            response.Should().NotBeNull();
+            //more tests
+        }
+
+        [Fact]
+        public async Task UpdateCustomerTest()
+        {
+#warning ToDo: Implement UpdateCustomerTest
+            Assert.True(false);
+        }
+
+        [Fact]
+        public async Task DeleteCustomerTest()
+        {
+#warning ToDo: Implement DeleteCustomerTest        
+            Assert.True(false);
+        }
+
+        [Fact]
+        public async Task ValidationTest()
+        {
+            var newCustomer = new Customer()
+            {
+                EmailAddress = "test@testing.com",
+                FirstName = "Test",
+                LastName = "Testing",
+                MiddleName = "Tested",
+            };
+
+            var jsonContent = JsonSerializer.Serialize(newCustomer);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("/api/Customer", content);
+            if (response.IsSuccessStatusCode)
+            {
+                Assert.Fail("Api call should have failed validation check.");
+            }
+            else
+            {
+                var errorResponse = await response.Content.ReadAsStringAsync();
+                response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+                //check additional validation error messages
+            }
+        }
     }
 }
---
+++ CustomerAPI01/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using CustomerAPI01.Data;
+using CustomerAPI01.Data.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerAPI01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController(CustomerRepository customerRepository, ILogger<CustomerController> logger, ActivitySource activitySource) 
+        {
+        }
+
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Get(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Post([FromBody] CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Put(Guid customerId, [FromBody] Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
